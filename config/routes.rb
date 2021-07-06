c1fea78184eef50da3d10947f4029b327d9b9967
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-07 00:29:43 +0300

    Add the capability of a user being able to resgister.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'main#index'
   get 'about_us', to: 'about#index', as: :about
   get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 end
