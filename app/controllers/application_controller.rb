5b6848bda67f1bb856996bafc0ce23e34939dbf4
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-08 20:40:16 +0300

    Add functionality to keep track of the current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def set_current_user
+        if session[:user_id]
+            Current.user.find_by(id: session[:user_id])
+        end
+    end
 end
