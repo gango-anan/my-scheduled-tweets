3363cca4b1d0a6938253df5ee74098a708ede7fb
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-08 22:36:08 +0300

    Add functionality to edit password.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
             Current.user = User.find_by(id: session[:user_id])
         end
     end
+
+    def require_user_logged_in!
+        redirect_to sign_in_path, alert: 'You must be logged in to make any edits' if Current.user.nil?
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
 
+  get 'password', to: 'passwords#edit', as: :edit_password
+  patch 'password', to: 'passwords#update'
+
   delete 'logout', to: 'sessions#destroy'
 end
