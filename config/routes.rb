ed8cd705f66e92ba53e7f19c48533dc12a224bf2
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-08 20:28:21 +0300

    Add signin feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'main#index'
   get 'about_us', to: 'about#index', as: :about
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
   delete 'logout', to: 'sessions#destroy'
 end
