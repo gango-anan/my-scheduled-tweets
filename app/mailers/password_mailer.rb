fe677bef02b33364217cc036e1c1324fce19f142
Galiwango Ananiya <galiwango101@gmail.com>
2021-07-09 01:00:07 +0300

    Implement functionality in  action mailer to send password reset emails.

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -6,8 +6,8 @@ class PasswordMailer < ApplicationMailer
   #   en.password_mailer.reset.subject
   #
   def reset
-    @greeting = "Hi"
+    @token  = params[:user].signed_id(purpose: 'password_reset', expires_in: 15.minutes)
 
-    mail to: "to@example.org"
+    mail to: params[:user].email
   end
 end
